f9cc6eb1cc7c533770297e93b0f893961d341920
PeterOwira <92856626+PeterOwira@users.noreply.github.com>
2023-09-10 20:14:16 +0000

    question_1

--- Question1.py
+++ Question1.py
@@ -1,1 +1,14 @@
+# Function to split a sentence into words
+def split_sentence(sentence):
+    words = sentence.split()
+    return words
 
+# Input sentence
+input_sentence = input("Enter an English sentence: ")
+
+# Split the sentence into words
+words = split_sentence(input_sentence)
+
+# Output individual words on separate lines
+for word in words:
+    print(word)
